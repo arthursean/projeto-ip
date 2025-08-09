dfbde09008a406c2958495a9d20fbed067802226
arthursean <arthurscampos0@gmail.com>
2025-08-09 12:53:35 -0300

    mapa e caminhos

--- main.py
+++ main.py
@@ -14,15 +14,19 @@ def create_tower(x, y):
                 torretas.add(torre.Torre((grid_x, grid_y)))
 #pra rodar no pygame so tirar o # desse trecho aq embaixo
 PATH = [
-(0, 192),    # entra pela esquerda
-(192, 192),  # vai para direita
-(192, 128),  # sobe
-(320, 128),  # vai para direita
-(320, 256),  # desce
-(384, 256),
-(448, 256),
-(448, 320),
-(704, 320),  # vai para direita (saída)
+(0, 224),    # entra pela esquerda
+(352, 224),  # vai para direita
+(352, 96),  # sobe
+(480, 96),  # vai para direita
+(480, 224),  # desce
+(352, 224), #esquerda
+(352, 480), #desce
+(224, 480),
+(224, 352),
+(928, 352),
+(928, 160),
+(1120, 160),
+(1120, 662)  # vai para direita (saída)
 ]
 
 
