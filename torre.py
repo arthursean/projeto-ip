956796fadf9001734c03fa8c23616480a5dfe598
arthursean <arthurscampos0@gmail.com>
2025-08-10 20:54:36 -0300

    comecando a animar

--- torre.py
+++ torre.py
@@ -6,20 +6,25 @@ import constantes as c
 class Torre(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.path = "imagens/teste.png"
+        
+        self.animacao = self.load()
+        self.pos_animacao = 0
+        self.tempo_animacao = pygame.time.get_ticks()
+
         self.tile = pos
         self.location = ((pos[0] + 0.5) * c.tileSize, (pos[1] + 0.5) * c.tileSize)
+
         self.range = DADOS['range']
         self.dano = DADOS['dmg']
         self.cd = DADOS['cooldown']
+
+        self.img = self.animacao[0]
         self.ultimo_tiro = pygame.time.get_ticks()
         self.som = pygame.mixer.Sound('som_tiro.mp3')
         self.angulo = 0
         self.selecionado = False
         
     def draw(self, screen):
-        self.img = pygame.image.load(self.path)
-        self.img = pygame.transform.scale_by(self.img, 1.1)
         self.img = pygame.transform.rotate(self.img, self.angulo)
         self.img_location = self.img.get_rect()
         self.img_location.center = self.location
@@ -36,11 +41,28 @@ class Torre(pygame.sprite.Sprite):
                     inimigo.dano(self.dano)
                     self.ultimo_tiro = pygame.time.get_ticks()
                     self.som.play()
-                    self.angulo = math.degrees(math.atan2(-dist_y, dist_x) - 90) #subtrai - 90 pois a imagem original é pra cima
+                    self.play_animation()
+                    #self.angulo = math.degrees(math.atan2(-dist_y, dist_x) - 90) #subtrai - 90 pois a imagem original é pra cima
                     break
+    def load(self):
+        sprite_sheet = pygame.image.load("imagens/sheet_caranguejo.png")
+        size = sprite_sheet.get_height()
+        animation_list = []
+        for x in range(7):
+            temp_img = sprite_sheet.subsurface(x * size, 0, size, size)
+            animation_list.append(temp_img)
+        return animation_list
+    def play_animation(self):
+        self.img = self.animacao[self.pos_animacao]
+        if pygame.time.get_ticks() - self.tempo_animacao > 30:
+            self.tempo_animacao = pygame.time.get_ticks()
+            self.pos_animacao += 1
+        if self.pos_animacao >= len(self.animacao):
+            self.pos_animacao = 0
     def update(self, inimigos):
         if pygame.time.get_ticks() - self.ultimo_tiro >= self.cd:
                 self.atacar(inimigos)
+                self.play_animation()
         elif pygame.time.get_ticks() - self.ultimo_tiro > 500:
             self.angulo = 0
     
